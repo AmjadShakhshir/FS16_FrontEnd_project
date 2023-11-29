71f418d0d2d507a3d7c29c8af3c70fa927fcf526
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-29 17:20:01 +0200

    add error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Root from './pages/Root/Root';
+import HomePage from './pages/Home/Home';
+import ProductsPage from './pages/Products/Products';
+import SingleProductPage from './pages/Product/Product';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        { 
+          path: '/',
+          element: <HomePage />
+        },
+        {
+          path: 'products',
+          element: <ProductsPage />
+        },
+        {
+          path: 'products/:id',
+          element: <SingleProductPage />,
+        },
+        {
+          path: 'register',
+          element: <RegisterPage />
+        },
+        {
+          path: 'login',
+          element: <LoginPage />
+        },
+      {
+        path:'profile',
+        element: <ProfilePage />
+      },
+      {
+        path: 'users',
+        element: <UsersPage />,
+      },
+      ]
+    },
+      {
+        path: '*',
+        element: <ErrorPage />
+      }
+  ]);
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
