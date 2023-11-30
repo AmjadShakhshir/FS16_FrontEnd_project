c550e45f038c16190e4e664e5d80dbbb48b94ece
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-30 21:35:45 +0200

    fix cart reducer2

--- src/redux/cartReducer.ts
+++ src/redux/cartReducer.ts
@@ -1,31 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { CartItem } from "../types/CartItem";
+import { Product } from "../types/Product";
 
-const initialState = {
-  products: [],
-};
+const initialState: CartItem[] = [];
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state, action) => {
-      const item = state.products.find((item) => item.id === action.payload.id);
-      if (item) {
-        item.quantity += action.payload.quantity;
+    addToCart: (state, action: PayloadAction<Product>) => {
+      const CartItem: CartItem = { ...action.payload, quantity: 1 };
+      const foundIndex = state.findIndex((item) => item.id === action.payload.id);
+      if (foundIndex !== -1) {
+        state[foundIndex].quantity++;
       } else {
-        state.products.push(action.payload);
+        state.push(CartItem);
       }
     },
-    removeItem: (state,action) => {
-      state.products=state.products.filter(item=>item.id !== action.payload)
+    removeItem: (state,action: PayloadAction<number>) => {
+      const productId = action.payload;
+      const foundIndex = state.findIndex((item) => item.id === productId);
+      if (foundIndex !== -1) {
+        state.splice(foundIndex,1);
+      }
     },
     resetCart: (state) => {
-      state.products = []
+      return initialState;
     },
   },
 });
 
+const cartReducer = cartSlice.reducer;
 // Action creators are generated for each case reducer function
 export const { addToCart,removeItem,resetCart } = cartSlice.actions;
 
-export default cartSlice.reducer;
+export default cartReducer;
