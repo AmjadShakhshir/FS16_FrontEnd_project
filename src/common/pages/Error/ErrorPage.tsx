c5c1f79c0cfbe3c9e2c6042759a6cc667c8654df
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-01-25 23:24:05 +0200

    edit Error Page and make it nicer

--- src/common/pages/Error/ErrorPage.tsx
+++ src/common/pages/Error/ErrorPage.tsx
@@ -1,8 +1,30 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import KeyboardDoubleArrowLeftIcon  from "@mui/icons-material/KeyboardDoubleArrowLeft"
+import { useNavigate } from 'react-router-dom'
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/", { replace: true });
+  };
   return (
-    <div>Page Not Found</div>
+    <Box m="30px 0">
+      <Typography>
+        404 Not Found 
+        <br />
+        The page you are looking for might have been removed or is temporarily
+        unavailable. Please check the URL and try again.
+      </Typography>
+      <Button
+        startIcon={<KeyboardDoubleArrowLeftIcon />}
+        onClick={handleClick}
+        sx={{ display: "flex", margin: "0 auto" }}
+      >
+        Go back
+      </Button>
+    </Box>
   )
 }
 
